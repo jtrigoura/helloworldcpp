be38bff3a97b69238e27e860affdc676bc7c7306
Jorge <jorgetrigoura1@gmail.com>
2025-12-18 00:01:10 -0500

    commented out main, in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
-    int x = 5;
-    // std::print("hello world {}", x);
-    std::cerr << "helloworld " << x << '\n';
-    std::vector<std::uint64_t> uints;
-    std::for_each(uints.begin(), uints.end(), [](const auto &i) { std::cout << i << " "; });
+// int main()
+// {
+//     int x = 5;
+//     // std::print("hello world {}", x);
+//     std::cerr << "helloworld " << x << '\n';
+//     std::vector<std::uint64_t> uints;
+//     std::for_each(uints.begin(), uints.end(), [](const auto &i) { std::cout << i << " "; });
 
-    return 0;
-}
+//     return 0;
+// }
